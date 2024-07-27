084dbdfeb9d1c29e23e87fbdddb83d30c3b544ea
Dev TtangKong <ttankkeo112@gmail.com>
2024-07-28 08:09:01 +0900

    , to ;

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,13 @@
 
 /** Signature for the interface that is defining the info of reorderable items. */
 export interface ReorderableItem<Key = string> {
-    key: Key,
-    element: HTMLElement
+    key: Key;
+    element: HTMLElement;
 }
 
 /** Signature for the interface that is defining the state of reorderable items. */
 export interface ReorderableState<Key = string> {
-    items: ReorderableItem<Key>[],
+    items: ReorderableItem<Key>[];
 }
 
 /** Signature for the interface that is defining changes to the reoderable state. */
